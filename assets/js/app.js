74d3bf296b67ce43ca3a3816297d3f46f6e86918
John Barreto <john.barreto@api-ux.com>
2022-05-31 19:38:07 -0500

    add animation active and fix bug sections active

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.controlls__control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 function pageTransitions() {
-    //Button Click active class
+    //Button click active class
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', function () {
             let currentBtn = document.querySelectorAll('.active-btn');
@@ -12,6 +12,26 @@ function pageTransitions() {
             this.className += ' active-btn';
         })
     }
+    //Sections Active
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+
+            e.target.classList.add('active');
+            //Remove selected from the other btns
+            sectBtn.forEach((btn) => {
+                btn.classList.remove('active');
+            })
+            //Hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+
+    })
 }
 
 pageTransitions();
