127cdeeb0ddaa132b8013bc2cb2e8932a4000fcf
John Barreto <john.barreto@api-ux.com>
2022-06-02 10:50:56 -0500

    remove and change img and add styles on scrollbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,10 @@ const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.controls__control');
 const allSections = document.querySelector('.main-content');
-
 function pageTransitions() {
     //Button click active class
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', function () {
-            console.log(sectBtn[i].attributes.data-id.value);
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
